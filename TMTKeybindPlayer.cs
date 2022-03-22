901608698f75f93fafdf1589398e581bcafdfa45
electrickiwi23 <66581831+electrickiwi23@users.noreply.github.com>
2022-03-22 08:20:51 -0700

    Did Code

--- TMTKeybindPlayer.cs
+++ TMTKeybindPlayer.cs
@@ -1,26 +1,34 @@
-﻿
-using Terraria;
-using Terraria.ID;
+﻿using Terraria;
 using Terraria.GameInput;
 using Terraria.ModLoader;
 using TooManyTabs.UI;
-
-
+using Terraria.UI;
 namespace TooManyTabs
 {
      class TMTKeybindPlayer : ModPlayer
     {
-        public override void  ProcessTriggers(TriggersSet triggersSet) { 
+        public override void ProcessTriggers(TriggersSet triggersSet) { 
 
             if (TooManyTabs.OpenKeybind.JustPressed)
             {
-                Main.NewText($"ExampleMod's ModKeybind was just pressed.");
+                if (!TooManyTabs.openedMenu)
+                {
+                    TMTSystem.searchMenu = new SearchMenu();
+
+                    TMTSystem.menuUserInterface = new UserInterface();
+                    TMTSystem.menuUserInterface.SetState(TMTSystem.searchMenu);
+
+                    TMTSystem.searchMenu.Activate();
+                    TooManyTabs.openedMenu = true;
+
+                    Item empty = new Item(0);
+                    Main.NewText(empty.Name);
+                }
 
                 SearchMenu.Visible = !SearchMenu.Visible;
 
-                Main.NewText(SearchMenu.Visible);
 
-
+                
             }
         }
 
---
+++ UI/TMTItemSlot.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.UI;
+using Terraria;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+
+
+
+
+namespace TooManyTabs.UI
+{
+    class TMTItemSlot : UIElement{
+
+        public Item item;
+        private float size;
+
+        public TMTItemSlot(Item item,float size) : base()
+        {
+            this.item = item;
+            this.size = size;
+            Width = new StyleDimension(size, 0f);
+            Height = new StyleDimension(size, 0f);
+     
+        }
+
+        public void setItem(Item item)
+        {
+            this.item = item;
+        }
+
+       
+
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            if (IsMouseHovering)
+            {
+             
+                ItemSlot.MouseHover(ref item,31);
+            }
+            
+            Vector2 position = GetDimensions().Center() + new Vector2(52f, 52f) * -0.5f * Main.inventoryScale;
+            Texture2D texture = TextureAssets.Item[item.type].Value;
+
+            Rectangle rect = base.GetDimensions().ToRectangle();
+            Rectangle itemRect = texture.Bounds;
+
+            itemRect.X = (int)(rect.X + (size - itemRect.Width)/2);
+            itemRect.Y = (int)(rect.Y + (size - itemRect.Height)/2);
+
+            Main.NewText(itemRect.Width);
+            Main.instance.LoadItem(item.type);
+            spriteBatch.Draw(TextureAssets.InventoryBack.Value,rect, Color.White);
+            spriteBatch.Draw(TextureAssets.Item[item.type].Value, itemRect, Color.White);
+            
+  
+        }
+
+    }
+}
